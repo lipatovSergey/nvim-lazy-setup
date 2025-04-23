8da582241f6e4ad7c78b3104ef5fbe467153ba0d
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-04-24 02:37:12 +0300

    treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "tsx",
+      "typescript",
+      "javascript",
+      "css",
+      "json",
+      "html",
+      "lua",
+      "markdown",
+      "markdown_inline",
+    },
+    highlight = {
+      enable = true,
+    },
+  },
+}
